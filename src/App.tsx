f8789ee8f890f04d98f923b05ac3028eae0ae225
Volodymyr Pavlenko <solaryasha@gmail.com>
2025-08-18 20:31:56 +0200

    update redirect to address

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,18 @@ import { Bookshelf } from './Bookshelf';
 import { Toaster } from './ui/toaster';
 import { Button } from './ui/button';
 
+const getURL = () => {
+  let url =
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    'http://localhost:3000/'
+  // Make sure to include `https://` when not localhost.
+  url = url.startsWith('http') ? url : `https://${url}`
+  // Make sure to include a trailing `/`.
+  url = url.endsWith('/') ? url : `${url}/`
+  return url
+}
+
 function App() {
   const [session, setSession] = useState<Session | null>(null);
 
@@ -25,6 +37,9 @@ function App() {
   const signUp = async () => {
     await supabase.auth.signInWithOAuth({
       provider: "google",
+      options: {
+        redirectTo: getURL();
+      }
     });
   };
 
